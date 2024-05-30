4c03d7fc5b38fc8c37b5e9cb051ff7009c19eca6
Volodymyr <dunkin.v@ajax.systems>
2024-05-30 21:32:13 +0300

    add url

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 export default function Books() {
   const [books, setBooks] = useState([])
   const fetchBooks = () => {
-    fetch("http://localhost:8000/api/cars")
+    fetch("https://basilisk-magical-easily.ngrok-free.app/api/cars")
       .then((resp) => resp.json())
       .then((data => setBooks(data)))
       .catch(error => console.log(error))
